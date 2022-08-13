1ab3b9a25d0647854dd758cfb69fe160016d1a7e
Koval <hazardsales@gmail.com>
2022-08-13 19:42:54 +0300

    update pom

--- src/main/java/org/fishbone/jpapractice/controllers/RegistrationController.java
+++ src/main/java/org/fishbone/jpapractice/controllers/RegistrationController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 import org.fishbone.jpapractice.models.Person;
 import org.fishbone.jpapractice.services.PersonDetailsService;
 import org.fishbone.jpapractice.utils.PersonValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationController.class);
 
     private final PersonDetailsService personDetailsService;
     private final PersonValidator personValidator;
@@ -24,12 +27,15 @@ public class RegistrationController {
 
     @PostMapping("/reg")
     public String register(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        LOGGER.debug("Start: register()");
 
         personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors()) {
             return "/auth/registration";
         }
         personDetailsService.register(person);
+
+        LOGGER.debug("End: register()");
         return "redirect:/auth/login";
     }
 }
