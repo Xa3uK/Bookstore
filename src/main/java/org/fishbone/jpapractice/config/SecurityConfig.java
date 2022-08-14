b9b222a3492477d9d16e32d4c3b6e9a53e05d4c6
Koval <hazardsales@gmail.com>
2022-08-14 13:39:55 +0300

    added csrf token in forms

--- src/main/java/org/fishbone/jpapractice/config/SecurityConfig.java
+++ src/main/java/org/fishbone/jpapractice/config/SecurityConfig.java
@@ -23,8 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
-            .authorizeRequests()
+        http.authorizeRequests()
             .antMatchers( "/books/delete", "/books/create").hasRole("ADMIN")
             .antMatchers("/auth/login", "/auth/registration", "/registration/reg", "/error").permitAll()
             .anyRequest().hasAnyRole("ADMIN", "USER")
